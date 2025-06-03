8bdece0a697670ca0c9b52f226895db8e752572e
Gustavo Ribeiro <gugahribeiro@hotmail.com>
2025-06-03 15:49:18 -0300

    chore: update header title

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -6,7 +6,7 @@ export function Header() {
   return (
     <nav className="fixed top-0 w-full z-50 bg-background/50 border-b border-white/10 backdrop-blur supports-[backdrop-filters]:bg-background/60 ">
       <div className="max-w-6xl mx-auto px-6 py-4 flex justify-between items-center">
-        <div className="text-2xl font-bold text-gray-500">INKWELL STUDIO</div>
+        <div className="text-2xl font-bold text-foreground">INK<span className="text-foreground/50">WELL</span> STUDIO</div>
         <NavMenu />
         <div className="flex gap-2">
           <Button variant='outline'>
